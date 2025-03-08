a74a8c9b00b6bcadab03678aec2f5147fec7e92b
Sebastian Wesołowski <Sebastian.Wesolowski@grupawp.pl>
2025-03-08 21:23:23 +0100

    Interface basics --- http app

--- main.go
+++ main.go
@@ -2,31 +2,42 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"os"
 )
 
-type bot interface {
-	getGreeting() string
-}
-
-type englishBot struct{}
-type spanishBot struct{}
+//type bot interface {
+//	getGreeting() string
+//}
+//
+//type englishBot struct{}
+//type spanishBot struct{}
 
 func main() {
-	eb := englishBot{}
-	sb := spanishBot{}
+	//eb := englishBot{}
+	//sb := spanishBot{}
+	//
+	//printGreeting(eb)
+	//printGreeting(sb)
 
-	printGreeting(eb)
-	printGreeting(sb)
+	// HTTP APPLICATION
+	resp, err := http.Get("http://www.google.com")
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(resp)
 }
 
-func printGreeting(b bot) {
-	fmt.Println(b.getGreeting())
-}
-
-func (eb englishBot) getGreeting() string {
-	return "Hello, World!"
-}
-
-func (sb spanishBot) getGreeting() string {
-	return "Holla!"
-}
+//func printGreeting(b bot) {
+//	fmt.Println(b.getGreeting())
+//}
+//
+//func (eb englishBot) getGreeting() string {
+//	return "Hello, World!"
+//}
+//
+//func (sb spanishBot) getGreeting() string {
+//	return "Holla!"
+//}
